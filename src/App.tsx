74fc1b6ec613772af9c5e36666af7ea139dcf460
Jiri Kozel <jirik@users.noreply.github.com>
2025-05-20 09:19:20 +0200

    Add openlayers

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,30 @@
+import 'ol/ol.css';
 import './App.css';
+import OlMap from 'ol/Map.js';
+import View from 'ol/View.js';
+import TileLayer from 'ol/layer/Tile.js';
+import OSM from 'ol/source/OSM.js';
+import { useEffect } from 'react';
 
 const App = () => {
-  return (
-    <div className="content">
-      <h1>Rsbuild with React</h1>
-      <p>Start building amazing things with Rsbuild.</p>
-    </div>
-  );
+  useEffect(() => {
+    const map = new OlMap({
+      target: 'map',
+      layers: [
+        new TileLayer({
+          source: new OSM(),
+        }),
+      ],
+      view: new View({
+        center: [0, 0],
+        zoom: 3,
+      }),
+    });
+    return () => {
+      map.setTarget(undefined);
+    }
+  }, []);
+  return <div id="map" />;
 };
 
 export default App;
